a8387239d7bebbb0b15f7f47c0cf17e2e309c62d
Gustavo Barragan <dev@droni.co>
2024-09-14 03:05:59 -0500

    support for mdi icons and base layout login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.blank')
 
 @section('content')
-<a href="{{ route('auth.redirect', 'google') }}">Login with google</a>
+<v-container>
+  <v-row justify="center">
+    <v-col cols="4">
+      <v-card>
+        <v-card-title>Login</v-card-title>
+        <v-card-text>
+          <v-btn
+            href="{{ route('auth.redirect', 'google') }}"
+            append-icon="mdi-google">
+            Login with google
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</v-container>
+
 @endsection
 
