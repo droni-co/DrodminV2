1ac68e3b4f78818501243a883e793ec0956805b9
Gustavo Barragan <dev@droni.co>
2024-09-14 01:56:41 -0500

    socialite support

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+  public function redirect($provider)
+  {
+    return Socialite::driver($provider)->redirect();
+  }
+
+  public function callback($provider)
+  {
+    $user = Socialite::driver($provider)->user();
+    dd($user);
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\AuthController;
+ 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-// Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Authentication Routes
+Route::get('/auth/{provider}/redirect', [AuthController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [AuthController::class, 'callback']);
