76d4f525f6c200364f0dacfd276f452c450c2f8e
Gustavo Barragan <dev@droni.co>
2024-09-26 01:43:34 -0500

    layout attachments

--- app/Http/Controllers/SiteAttachmentController.php
+++ app/Http/Controllers/SiteAttachmentController.php
@@ -5,54 +5,57 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Attachment;
 use App\Models\Site;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class SiteAttachmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index($siteId, Request $request)
-    {
-      $site = Site::findOrFail($siteId);
-      $attachments = Attachment::where('site_id', $siteId)->orderBy('created_at', 'desc')->paginate(24);
-      return view('sites.attachments.index', compact('site', 'attachments'));
+  /**
+   * Display a listing of the resource.
+   */
+  public function index($siteId, Request $request)
+  {
+    $site = Site::findOrFail($siteId);
+    $attachments = Attachment::where('site_id', $siteId);
+    if($request->has('search')) {
+      $attachments = $attachments->where('name', 'like', '%'.$request->search.'%');
     }
+    $attachments = $attachments->orderBy('created_at', 'desc')->paginate(24);
+    return view('sites.attachments.index', compact('site', 'attachments'));
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store($siteId, Request $request)
-    {
-      $request->validate([
-        'file' => 'required|file|max:1024|mimes:gif,png,jpg,jpeg,pdf,doc,docx,xls,xlsx,ppt,pptx',
-      ]);
-      $attachment = new Attachment();
-      $attachment->user_id = Auth::user()->id;
-      $attachment->site_id = $siteId;
-      $attachment->name = $request->file('file')->getClientOriginalName();
-      $attachment->path = Storage::disk('digitalocean')->putFile($siteId.'/'.Auth::user()->id, request()->file, 'public');
-      $attachment->size = $request->file('file')->getSize();
-      $attachment->extension = $request->file('file')->extension();
-      $attachment->mime_type = $request->file('file')->getMimeType();
-      $attachment->save();
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store($siteId, Request $request)
+  {
+    $request->validate([
+      'file' => 'required|file|max:1024|mimes:gif,png,jpg,jpeg,pdf,doc,docx,xls,xlsx,ppt,pptx',
+    ]);
+    $attachment = new Attachment();
+    $attachment->user_id = Auth::user()->id;
+    $attachment->site_id = $siteId;
+    $attachment->name = $request->file('file')->getClientOriginalName();
+    $attachment->path = Storage::disk('digitalocean')->putFile($siteId.'/'.Auth::user()->id, request()->file, 'public');
+    $attachment->size = $request->file('file')->getSize();
+    $attachment->extension = $request->file('file')->extension();
+    $attachment->mime_type = $request->file('file')->getMimeType();
+    $attachment->save();
 
-      flash('Attachment uploaded successfully.')->success();
-      return redirect()->route('sites.attachments.index', $siteId);
-    }
+    flash('Attachment uploaded successfully.')->success();
+    return redirect()->route('sites.attachments.index', $siteId);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($siteId, string $id)
-    {
-      $attachment = Attachment::where('site_id', $siteId)->findOrFail($id);
-      Storage::disk('digitalocean')->delete($attachment->path);
-      $attachment->delete();
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy($siteId, string $id)
+  {
+    $attachment = Attachment::where('site_id', $siteId)->findOrFail($id);
+    Storage::disk('digitalocean')->delete($attachment->path);
+    $attachment->delete();
 
-      flash('Attachment deleted successfully.')->success();
-      return redirect()->route('sites.attachments.index', $siteId);
-    }
+    flash('Attachment deleted successfully.')->success();
+    return redirect()->route('sites.attachments.index', $siteId);
+  }
 }
--- resources/views/sites/attachments/index.blade.php
+++ resources/views/sites/attachments/index.blade.php
@@ -4,7 +4,10 @@
   <div class="container">
     <div class="border-bottom py-3 d-flex">
       <h1>Multimedia</h1>
-      <div class="flex-grow-1 text-end pt-1">
+      <div class="flex-grow-1 text-end pt-1 d-flex justify-content-end">
+        <form action="" method="GET" class="px-3">
+          <input type="search" name="search" class="form-control rounded-pill" placeholder="Buscar...">
+        </form>
         <form action="{{ route('sites.attachments.store', $site) }}" method="POST" enctype="multipart/form-data">
           @csrf
           <input type="file" name="file" id="file" class="d-none" onchange="this.form.submit()">
@@ -15,21 +18,38 @@
         </form>
       </div>
     </div>
-    <div class="row">
-      @foreach ($attachments as $attachment)
-        <div class="col-md-2">
-          <div class="card">
-            <img src="{{ Storage::disk('digitalocean')->url($attachment->path) }}" class="card-img-top" alt="{{ $attachment->name }}">
-            <form action="{{ route('sites.attachments.destroy', [$site, $attachment]) }}" method="POST">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">
-                <i class="mdi mdi-delete"></i>
-              </button>
-            </form>
+    <div class="row my-3">
+      @foreach($attachments as $attachment)
+        <div class="col-md-3">
+          <div class="card shadow">
+            <a href="{{ Storage::disk('digitalocean')->url($attachment->path) }}" target="_blank">
+              @if(Str::startsWith($attachment->mime_type, 'image'))
+                <img src="{{ Storage::disk('digitalocean')->url($attachment->path) }}" class="card-img-top" alt="{{ $attachment->name }}">
+              @else
+                <img src="/img/docFile.png" class="card-img-top" alt="{{ $attachment->name }}">
+              @endif
+            </a>
+            <div class="card-body d-flex">
+              <div class="flex-grow-1">
+                <h5 class="m-0">{{ $attachment->name }}</h5>
+                <small class="text-secondary">
+                  <i class="mdi mdi-calendar"></i> {{ $attachment->created_at->diffForHumans() }}<br>
+                  <i class="mdi mdi-sd"></i> {{ Number::fileSize($attachment->size) }}<br>
+                </small>
+              </div>
+              <form action="{{ route('sites.attachments.destroy', [$site, $attachment]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-sm btn-outline-danger">
+                  <i class="mdi mdi-delete"></i>
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       @endforeach
+
+      {{ $attachments->links() }}
 
   </div>
 @endsection
