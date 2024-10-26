34ce11c98678e348c3cb29603e581c05e4c7e8c6
Gustavo Barragan <dev@droni.co>
2024-10-26 13:11:00 -0500

    tabs in post form

--- resources/views/sites/posts/_form.blade.php
+++ resources/views/sites/posts/_form.blade.php
@@ -1,49 +1,94 @@
-<div class="form-floating mb-3">
-  <input type="text" class="form-control" id="name" name="name" placeholder="Título del post" required value="{{ old('name', $post->name) }}">
-  <label for="name">Título del post</label>
-</div>
-<div class="form-floating mb-3">
-  <textarea class="form-control" placeholder="Descripción" id="description" name="description" style="height: 100px;">{{ old('description', $post->description) }}</textarea>
-  <label for="floatingTextarea">Descripción</label>
-</div>
-<div class="mb-3">
-  <label for="picture">Imagen</label>
-  <attachment-input site-id="{{ $site->id }}" name="picture" accept="" value="{{ old('picture', $post->picture) }}"></attachment-input>
-</div>
-<div class="mb-3">
-  <h5>Categorías</h5>
-  <ul class="list-group">
-    @foreach ($categories as $category)
-    <li class="list-group-item">
-      <input
-        class="form-check-input me-1"
-        type="checkbox"
-        id="category-{{ $category->id }}"
-        name="categories[]"
-        value="{{ $category->id }}"
-        {{ in_array($category->id, old('categories', $post->categories->pluck('id')->toArray())) ? 'checked' : '' }}>
-      <label class="form-check-label" for="category-{{ $category->id }}">
-        {{ $category->name }}
-        <small class="text-muted">/{{ $category->slug }}</small>
-      </label>
-    </li>
-    @endforeach
-  </ul>
-</div>
-<div class="form-floating mb-3">
-  <select class="form-select" id="format" name="format">
-    <option value="html" {{ old('format', $post->format) == 'html' ? 'selected' : '' }}>HTML</option>
-    <option value="markdown" {{ old('format', $post->format) == 'markdown' ? 'selected' : '' }}>Markdown</option>
-  </select>
-  <label for="format">Formato</label>
-</div>
-<div class="form-check form-switch mb-3">
-  <input class="form-check-input" type="checkbox" id="active" name="active" value="1" {{ old('active', $post->active) ? 'checked' : '' }}>
-  <label class="form-check-label" for="active">¿Está activo?</label>
-</div>
-<div class="form-floating mb-3">
-  <button type="submit" class="btn btn-primary">
-    <i class="mdi mdi-content-save"></i>
-    Guardar
-  </button>
+<ul class="nav nav-tabs" id="myTab">
+  <li class="nav-item">
+    <button
+      class="nav-link active"
+      id="postInfo-tab"
+      data-bs-toggle="tab"
+      data-bs-target="#postInfo"
+      type="button"
+      role="tab"
+      aria-controls="postInfo"
+      aria-selected="true">
+      Información
+    </button>
+  </li>
+  <li class="nav-item">
+    <button
+      class="nav-link"
+      id="postCategories-tab"
+      data-bs-toggle="tab"
+      data-bs-target="#postCategories"
+      type="button"
+      role="tab"
+      aria-controls="postCategories"
+      aria-selected="false">
+      Categorías
+    </button>
+  </li>
+  <li class="nav-item">
+    <button
+      class="nav-link"
+      id="postProps-tab"
+      data-bs-toggle="tab"
+      data-bs-target="#postProps"
+      type="button"
+      role="tab"
+      aria-controls="postProps"
+      aria-selected="false">
+      Propiedades
+    </button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="postInfo" role="tabpanel" aria-labelledby="postInfo-tab">
+    <!-- Tab de información -->
+    <div class="form-floating mb-3 mt-2">
+      <input type="text" class="form-control" id="name" name="name" placeholder="Título del post" required value="{{ old('name', $post->name) }}">
+      <label for="name">Título del post</label>
+    </div>
+    <div class="form-floating mb-3">
+      <textarea class="form-control" placeholder="Descripción" id="description" name="description" style="height: 100px;">{{ old('description', $post->description) }}</textarea>
+      <label for="floatingTextarea">Descripción</label>
+    </div>
+    <div class="mb-3">
+      <label for="picture">Imagen</label>
+      <attachment-input site-id="{{ $site->id }}" name="picture" accept="" value="{{ old('picture', $post->picture) }}"></attachment-input>
+    </div>
+    <div class="form-floating mb-3">
+      <select class="form-select" id="format" name="format">
+        <option value="html" {{ old('format', $post->format) == 'html' ? 'selected' : '' }}>HTML</option>
+        <option value="markdown" {{ old('format', $post->format) == 'markdown' ? 'selected' : '' }}>Markdown</option>
+      </select>
+      <label for="format">Formato</label>
+    </div>
+    <div class="form-check form-switch mb-3">
+      <input class="form-check-input" type="checkbox" id="active" name="active" value="1" {{ old('active', $post->active) ? 'checked' : '' }}>
+      <label class="form-check-label" for="active">¿Está activo?</label>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="postCategories" role="tabpanel" aria-labelledby="postCategories-tab">
+    <div class="mb-3 mt-2">
+      <ul class="list-group">
+        @foreach ($categories as $category)
+        <li class="list-group-item">
+          <input
+            class="form-check-input me-1"
+            type="checkbox"
+            id="category-{{ $category->id }}"
+            name="categories[]"
+            value="{{ $category->id }}"
+            {{ in_array($category->id, old('categories', $post->categories->pluck('id')->toArray())) ? 'checked' : '' }}>
+          <label class="form-check-label" for="category-{{ $category->id }}">
+            {{ $category->name }}
+            <small class="text-muted">/{{ $category->slug }}</small>
+          </label>
+        </li>
+        @endforeach
+      </ul>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="postProps" role="tabpanel" aria-labelledby="postProps-tab">
+    Propiedades
+  </div>
+    
 </div>
--- resources/views/sites/posts/edit.blade.php
+++ resources/views/sites/posts/edit.blade.php
@@ -12,15 +12,28 @@
       format="{{ old('format', $post->format) }}"
       name="content">
     </monaco-editor>
-    <div class="col-md-5 col-lg-4 py-3">
-      @include('sites.posts._form', [$post, $categories])
-
-      <div class="input-group mb-3">
-        <input type="text" form="delete" class="form-control is-invalid" name="validator" placeholder="Escribe '{{ $post->slug }}' para borrar">
-        <button type="submit" form="delete" class="btn btn-sm btn-outline-danger">
-          <i class="mdi mdi-delete"></i>
-          Eliminar
-        </button>
+    <div class="col-md-5 col-lg-4 py-3 d-flex flex-column border">
+      <div class="flex-grow-1">
+        @include('sites.posts._form', [$post, $categories])
+      </div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <div class="form-floating mb-3">
+            <button type="submit" class="btn btn-primary">
+              <i class="mdi mdi-content-save"></i>
+              Guardar
+            </button>
+          </div>
+        </div>
+        <div class="text-end">
+          <div class="input-group mb-3">
+            <input type="text" form="delete" class="form-control is-invalid" name="validator" placeholder="Escribe '{{ $post->slug }}' para borrar">
+            <button type="submit" form="delete" class="btn btn-sm btn-outline-danger">
+              <i class="mdi mdi-delete"></i>
+              Eliminar
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </form>
