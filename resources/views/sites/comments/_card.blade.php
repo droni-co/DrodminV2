d2525ad7be81cd5dc1c9e53fe161089d2b5fb50a
Gustavo Barragan <dev@droni.co>
2024-10-29 20:57:02 -0500

    comment partial

--- resources/views/sites/comments/_card.blade.php
+++ resources/views/sites/comments/_card.blade.php
@@ -1,13 +1,42 @@
-<div class="card">
+<div class="card mb-2">
   <div class="p-2">
-    <small>
-      <i class="mdi mdi-account"></i>
-      {{ $comment->user->name }}
-      <i class="mdi mdi-pencil-box-multiple-outline"></i>
-      {{ $comment->commentable->name }}
-      <i class="mdi mdi-calendar"></i>
-      {{ $comment->created_at }}
-    </small>
+    <div class="d-flex">
+      <div class="flex-grow-1">
+        <small class="{{ $comment->approved_at ? '' : 'text-danger' }}">
+          {{ $comment->approved_at ? '' : 'Pendiente' }}
+          <i class="mdi mdi-account"></i>
+          {{ $comment->user->name }}
+          <i class="mdi mdi-pencil-box-multiple-outline"></i>
+          {{ $comment->commentable->name }}
+          <i class="mdi mdi-calendar"></i>
+          {{ $comment->created_at }}
+        </small>
+      </div>
+      <div class="dropdown">
+        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="mdi mdi-cog-outline"></i>
+        </button>
+        <ul class="dropdown-menu dropdown-menu-lg-end">
+          @if(!$comment->approved_at)
+          <li>
+            <form action="{{ route('sites.comments.update', [$site, $comment]) }}" method="POST">
+              @csrf
+              @method('PATCH')
+              <input type="hidden" name="approved_at" value="{{ $comment->approved }}">
+              <button type="submit" class="dropdown-item">Aprobar</button>
+            </form>
+          </li>
+          @endif
+          <li>
+            <form action="{{ route('sites.comments.destroy', [$site, $comment]) }}" method="POST">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="dropdown-item text-danger">Eliminar</button>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </div>
     @if($comment->parent)
     <div class="text-secondary">
       <strong>Respuesta a:</strong>
@@ -15,34 +44,5 @@
     </div>
     @endif
     <pre>{{ $comment->content }}</pre>
-
-    <form action="{{ route('sites.comments.destroy', [$site, $comment]) }}" method="POST">
-      @csrf
-      @method('DELETE')
-      <button type="submit" class="btn btn-sm btn-danger">Eliminar</button>
-    </form>
-
-    
-    
-    @if(!$comment->approved_at)
-    <form action="{{ route('sites.comments.update', [$site, $comment]) }}" method="POST">
-      @csrf
-      @method('PATCH')
-      <input type="hidden" name="approved_at" value="{{ $comment->approved }}">
-      <button type="submit" class="btn btn-sm btn-{{ $comment->approved_at ? 'danger' : 'success' }}">
-        {{ $comment->approved_at ? 'Desaprobar' : 'Aprobar' }}
-      </button>
-    </form>
-    @endif
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
   </div>
 </div>
--- resources/views/sites/comments/index.blade.php
+++ resources/views/sites/comments/index.blade.php
@@ -5,58 +5,9 @@
     <div class="border-bottom py-3 d-flex">
       <h1>Comentarios</h1>
     </div>
-    <table class="table table-striped table-hover">
-      <thead>
-        <tr>
-          <th>Info</th>
-          <th>Comentario</th>
-          <th>TimeStamps</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($comments as $comment)
-        <tr>
-          <td>
-            <a href="{{ route('sites.posts.edit', [$site, $comment->commentable_id]) }}">
-              En: <strong>{{ $comment->commentable->name }}</strong>
-            </a><br>
-            Usuario: <strong>{{ $comment->user->name }}</strong>
-
-            <form action="{{ route('sites.comments.destroy', [$site, $comment]) }}" method="POST">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-sm btn-danger">Eliminar</button>
-            </form>
-          </td>
-          <td>
-            @if($comment->parent)
-            <div class="text-secondary">
-              <strong>Respuesta a:</strong>
-              <pre>{{ $comment->parent->content }}</pre>
-            </div>
-            @endif
-            <pre>{{ $comment->content }}</pre>
-            @if(!$comment->approved_at)
-            <form action="{{ route('sites.comments.update', [$site, $comment]) }}" method="POST">
-              @csrf
-              @method('PATCH')
-              <input type="hidden" name="approved_at" value="{{ $comment->approved }}">
-              <button type="submit" class="btn btn-sm btn-{{ $comment->approved_at ? 'danger' : 'success' }}">
-                {{ $comment->approved_at ? 'Desaprobar' : 'Aprobar' }}
-              </button>
-            </form>
-            @endif
-          </td>
-          <td class="{{ !$comment->approved_at ? 'bg-danger text-white' : '' }}">
-            <small>
-              <i class="mdi mdi-calendar"></i> {{ $comment->created_at }}<br>
-              <i class="mdi mdi-update"></i> {{ $comment->updated_at }}
-            </small>
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
+    @foreach ($comments as $comment)
+      @include('sites.comments._card', ['comment' => $comment])
+    @endforeach
     {{ $comments->links() }}
   </div>
 @endsection
